04739a1a548714473aa47f77a55a48fab7293343
unknown <christopherleon237@gmail.com>
2022-07-06 23:53:31 +0100

    first commit

---
+++ blog/blogapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostList,PostDetail,UserDetail,UserList
+
+
+urlpatterns=[
+    path('',PostList.as_view()),
+    path('<int:pk>/',PostDetail.as_view()),
+    path('users/',UserList.as_view()),
+    path('users/<int:pk>/',UserDetail.as_view()),
+]
