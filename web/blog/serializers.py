71a2ab0f044fe943dc91ebee7c0b10f6c57ed322
AFRIKAN BACKEND DEVELOPER <104680796+CaptainLeon445@users.noreply.github.com>
2022-07-07 08:54:14 +0100

    Update serializers.py

--- web/blog/serializers.py
+++ web/blog/serializers.py
@@ -1,17 +1,1 @@
-from rest_framework import serializers
-from .models import Comment,Post,Category
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Category
-        fields=['id','name','slug']
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Post
-        fields=['id','title','slug','author','body','category','tags','created','updated']
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Comment
-        fields=['id','name','body','post','created']
